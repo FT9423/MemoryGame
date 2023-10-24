8969bcd9c5069a9922f19472e2bbd4bd685b603e
FT9423 <fernandohi982@outlook.com>
2023-10-23 19:17:11 -0500

    Added notes

--- src/memoryGame.java
+++ src/memoryGame.java
@@ -1,12 +1,16 @@
+//Stores list and that list to be manipulated
 import java.util.ArrayList;
+//Functions as a wrapper
 import java.util.Collections;
+//Takes user input
 import java.util.Scanner;
 
 public class memoryGame {
 
     public static void main (String[] args){
-
+        //Taking user input
         Scanner scanner = new Scanner(System.in);
+        //Creating list of card values
         ArrayList<String> cards = new ArrayList<>();
         cards.add("A");
         cards.add("A");
@@ -16,12 +20,16 @@ public class memoryGame {
         cards.add("C");
         cards.add("D");
         cards.add("D");
+        //Randomize order of cards listed above
         Collections.shuffle(cards);
 
+        //String Array - Board = Size of cards list
         String[] board = new String[cards.size()];
+        //Which card is flipped using index - yes or no
         boolean[] flipped = new boolean[cards.size()];
         int pairsFound = 0;
 
+        //Stops game once all 4 pairs are found. Checks if first flipped card == second card flipped
         while (pairsFound < 4) {
             printBoard(board);
             int firstIndex = getCardIndex (
@@ -38,6 +46,7 @@ public class memoryGame {
                 System.out.println("You found a pair!");
                 pairsFound++;
             }
+            //Changes value of card from null to "" after cards are flipped without a match being found
             else {
                 System.out.println("Sorry, those cards do not match.");
                 board[firstIndex] = " ";
@@ -50,6 +59,7 @@ public class memoryGame {
         System.out.println("Congratulations, you won!");
     }
 
+    //Allowes user to choose card by index while giving errors for duplicate choices and invalid entries
     public static int getCardIndex(Scanner scanner,String[] board,boolean[] flipped,String prompt){
         int index;
         while (true) {
@@ -68,6 +78,7 @@ public class memoryGame {
         return index;
     }
 
+    //Prints the proper value for each card
     public static void printBoard(String[] board) {
         for (int i = 0; i < board.length; i++) {
             System.out.print("| " + board[i] + " ");
